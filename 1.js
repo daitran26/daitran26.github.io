0d5c22aef5e92ee298a6df59f73f70085ed03189
daitran26 <tranvandai6226.com>
2021-07-02 16:10:28 +0700

    upload 2-7

--- 1.js
+++ 1.js
@@ -1,4 +1,16 @@
 ﻿ $(function(){
- 
+     vitri = $('#khoisp').offset().top;
+    $(".chuot,.kn").click(function(){
+        $("body,html").animate({scrollTop:700},700)
+    })
+    $(".sp").click(function(){
+        $("body,html").animate({scrollTop:vitri},700)
+    })
+    $(".home").click(function(){
+        $("body,html").animate({scrollTop:0},700)
+    })
+    $(".lienhe").click(function(){
+        $("body,html").animate({scrollTop:$('#lien').offset().top},700)
+    })
 })  
  
