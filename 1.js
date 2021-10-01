fcfbd3202e0bfa9b4beafaa73bc034225a761946
daitran26 <tranvandai6226.com>
2021-10-01 08:28:43 +0700

    fix

--- 1.js
+++ 1.js
@@ -13,4 +13,16 @@
         $("body,html").animate({scrollTop:$('#lien').offset().top},700)
     })
 })  
- 
+document.addEventListener("DOMContentLoaded", function(){
+    var nut = document.querySelectorAll('.nav-item');
+    var line = document.querySelector('.line');
+    nut.forEach((item,index) => {
+        item.onclick = function(){
+            document.querySelector('.nav-item.active').classList.remove('active');
+            this.classList.add('active')
+            line.style.width = this.offsetWidth + 'px';
+            line.style.left = this.offsetLeft + 'px';
+        }
+    });
+    
+},false)
